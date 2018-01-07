40ccb2edb3c6602de6134a32d20a294832a706a6
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-01-08 00:00:00 +0100

    Fixed form factories.

--- src/Service/Form/BasicFormFactory.php
+++ src/Service/Form/BasicFormFactory.php
@@ -2,16 +2,15 @@
 namespace Search\Service\Form;
 
 use Interop\Container\ContainerInterface;
-use Zend\ServiceManager\Factory\FactoryInterface;
 use Search\Form\BasicForm;
+use Zend\ServiceManager\Factory\FactoryInterface;
 
 class BasicFormFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $form = new BasicForm;
+        $form = new BasicForm(null, $options);
         $form->setTranslator($services->get('MvcTranslator'));
-
         return $form;
     }
 }
