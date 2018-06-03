ea11fb187d37e6c3cad365cddf6fb273be4f2849
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-06-04 00:00:00 +0200

    Moved upgrades scripts into a separate file.

--- Module.php
+++ Module.php
@@ -126,84 +126,6 @@ SQL;
         $this->manageSiteSettings($serviceLocator, 'install');
     }
 
-    public function upgrade($oldVersion, $newVersion,
-        ServiceLocatorInterface $serviceLocator
-    ) {
-        /* @var \Doctrine\DBAL\Connection $connection */
-        $connection = $serviceLocator->get('Omeka\Connection');
-
-        if (version_compare($oldVersion, '0.1.1', '<')) {
-            $connection->exec('
-                ALTER TABLE search_page
-                CHANGE `form` `form_adapter` varchar(255) NOT NULL
-            ');
-        }
-
-        if (version_compare($oldVersion, '0.5.0', '<')) {
-            // There is no "drop foreign key if exists", so check it.
-            $sql = '';
-            $sm = $connection->getSchemaManager();
-            $keys = ['search_page_ibfk_1', 'index_id', 'IDX_4F10A34984337261', 'FK_4F10A34984337261'];
-            $foreignKeys = $sm->listTableForeignKeys('search_page');
-            foreach ($foreignKeys as $foreignKey) {
-                if ($foreignKey && in_array($foreignKey->getName(), $keys)) {
-                    $sql .= 'ALTER TABLE search_page DROP FOREIGN KEY ' . $foreignKey->getName() . ';' . PHP_EOL;
-                }
-            }
-            $indexes = $sm->listTableIndexes('search_page');
-            foreach ($indexes as $index) {
-                if ($index && in_array($index->getName(), $keys)) {
-                    $sql .= 'DROP INDEX ' . $index->getName() . ' ON search_page;' . PHP_EOL;
-                }
-            }
-
-            $sql .= <<<'SQL'
-ALTER TABLE search_index CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE settings settings LONGTEXT DEFAULT NULL COMMENT '(DC2Type:json_array)';
-ALTER TABLE search_page CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE index_id index_id INT NOT NULL AFTER id, CHANGE settings settings LONGTEXT DEFAULT NULL COMMENT '(DC2Type:json_array)';
-CREATE INDEX IDX_4F10A34984337261 ON search_page (index_id);
-ALTER TABLE search_page ADD CONSTRAINT search_page_ibfk_1 FOREIGN KEY (index_id) REFERENCES search_index (id);
-SQL;
-            $sqls = array_filter(array_map('trim', explode(';', $sql)));
-            foreach ($sqls as $sql) {
-                $connection->exec($sql);
-            }
-        }
-
-        if (version_compare($oldVersion, '0.5.1', '<')) {
-            // There is no "drop foreign key if exists", so check it.
-            $sql = '';
-            $sm = $connection->getSchemaManager();
-            $keys = ['search_page_ibfk_1', 'index_id', 'IDX_4F10A34984337261', 'FK_4F10A34984337261'];
-            $foreignKeys = $sm->listTableForeignKeys('search_page');
-            foreach ($foreignKeys as $foreignKey) {
-                if ($foreignKey && in_array($foreignKey->getName(), $keys)) {
-                    $sql .= 'ALTER TABLE search_page DROP FOREIGN KEY ' . $foreignKey->getName() . ';' . PHP_EOL;
-                }
-            }
-            $indexes = $sm->listTableIndexes('search_page');
-            foreach ($indexes as $index) {
-                if ($index && in_array($index->getName(), $keys)) {
-                    $sql .= 'DROP INDEX ' . $index->getName() . ' ON search_page;' . PHP_EOL;
-                }
-            }
-
-            $sql .= <<<'SQL'
-CREATE INDEX IDX_4F10A34984337261 ON search_page (index_id);
-ALTER TABLE search_page ADD CONSTRAINT FK_4F10A34984337261 FOREIGN KEY (index_id) REFERENCES search_index (id) ON DELETE CASCADE;
-SQL;
-            $sqls = array_filter(array_map('trim', explode(';', $sql)));
-            foreach ($sqls as $sql) {
-                $connection->exec($sql);
-            }
-        }
-
-        if (version_compare($oldVersion, '0.5.2', '<')) {
-            $settings = $serviceLocator->get('Omeka\Settings');
-            $this->manageSettings($settings, 'install', 'settings');
-            $this->manageSiteSettings($serviceLocator, 'install');
-        }
-    }
-
     public function uninstall(ServiceLocatorInterface $serviceLocator)
     {
         $sql = <<<'SQL'
@@ -215,6 +137,12 @@ SQL;
         foreach ($sqls as $sql) {
             $connection->exec($sql);
         }
+    }
+
+    public function upgrade($oldVersion, $newVersion,
+        ServiceLocatorInterface $serviceLocator)
+    {
+        require_once 'data/scripts/upgrade.php';
     }
 
     protected function manageSettings($settings, $process, $key = 'config')
