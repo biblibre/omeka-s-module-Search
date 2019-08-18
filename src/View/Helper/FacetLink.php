29eb20c1cda9fb7c7b01e78e942cc9b25d0395b9
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-08-19 00:00:00 +0200

    Improved helper FacetLink.

--- src/View/Helper/FacetLink.php
+++ src/View/Helper/FacetLink.php
@@ -7,23 +7,61 @@ use Zend\View\Helper\AbstractHelper;
 
 class FacetLink extends AbstractHelper
 {
+    /**
+     * @param Application $application
+     */
+    protected $application;
+
+    /**
+     * @param Application $application
+     */
     public function __construct(Application $application)
     {
         $this->application = $application;
     }
 
+    /**
+     * Create one facet link.
+     *
+     * @param string $name
+     * @param string $facet
+     * @return string
+     */
     public function __invoke($name, $facet)
     {
-        $mvcEvent = $this->application->getMvcEvent();
-        $routeMatch = $mvcEvent->getRouteMatch();
-        $request = $mvcEvent->getRequest();
+        // Variables are static to speed up process.
+        static $urlHelper;
+        static $partialHelper;
+        static $escapeHtml;
 
-        $route = $routeMatch->getMatchedRouteName();
-        $params = $routeMatch->getParams();
-        $query = $request->getQuery()->toArray();
+        static $mvcEvent;
+        static $routeMatch;
+        static $request;
 
-        $active = false;
-        if (isset($query['limit'][$name]) && false !== array_search($facet['value'], $query['limit'][$name])) {
+        static $route;
+        static $params;
+        static $queryBase;
+
+        if (is_null($mvcEvent)) {
+            $plugins = $this->getView()->getHelperPluginManager();
+            $urlHelper = $plugins->get('url');
+            $partialHelper = $plugins->get('partial');
+            $escapeHtml = $plugins->get('escapeHtml');
+
+            $mvcEvent = $this->application->getMvcEvent();
+            $routeMatch = $mvcEvent->getRouteMatch();
+            $request = $mvcEvent->getRequest();
+
+            $route = $routeMatch->getMatchedRouteName();
+            $params = $routeMatch->getParams();
+            $queryBase = $request->getQuery()->toArray();
+
+            unset($queryBase['page']);
+        }
+
+        $query = $queryBase;
+
+        if (isset($query['limit'][$name]) && array_search($facet['value'], $query['limit'][$name]) !== false) {
             $values = $query['limit'][$name];
             $values = array_filter($values, function ($v) use ($facet) {
                 return $v != $facet['value'];
@@ -32,18 +70,17 @@ class FacetLink extends AbstractHelper
             $active = true;
         } else {
             $query['limit'][$name][] = $facet['value'];
+            $active = false;
         }
 
-        unset($query['page']);
-
-        $view = $this->getView();
-        $url = $view->url($route, $params, ['query' => $query]);
-
-        return $view->partial('search/facet-link', [
-            'url' => $url,
-            'active' => $active,
+        return $partialHelper('search/facet-link', [
+            'name' => $name,
             'value' => $facet['value'],
+            'url' => $urlHelper($route, $params, ['query' => $query]),
+            'active' => $active,
             'count' => $facet['count'],
+            // To speed up process.
+            'escapeHtml' => $escapeHtml,
         ]);
     }
 }
