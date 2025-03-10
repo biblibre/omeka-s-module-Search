a8ee63867361d060bda55799f615ed3cc0368307
Thibaud Guillot <thibaud.guillot@biblibre.com>
2025-03-10 13:26:23 +0100

    Fix PHP warning when SavedQueryForm is construct

--- src/Service/Form/SaveQueryFormFactory.php
+++ src/Service/Form/SaveQueryFormFactory.php
@@ -9,7 +9,7 @@ class SaveQueryFormFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $form = new SaveQueryForm(null, $options);
+        $form = new SaveQueryForm(null, $options ?? []);
         $viewHelperManager = $services->get('ViewHelperManager');
         $form->setUrlHelper($viewHelperManager->get('Url'));
         return $form;
