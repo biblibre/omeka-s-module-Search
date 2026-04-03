b4379ab2b66bdda5c6435d9d87628d209a28b14d
Julian Maurice <julian.maurice@biblibre.com>
2026-04-03 08:50:24 +0200

    Fix description of check interval setting
    Script name was wrong
    
    Closes #44

--- src/Form/ConfigForm.php
+++ src/Form/ConfigForm.php
@@ -13,7 +13,7 @@ class ConfigForm extends Form
             'type' => 'Text',
             'options' => [
                 'label' => 'Check interval (seconds)', // @translate
-                'info' => 'The frequency at which the module checks if there are resources to be indexed. A smaller interval means a more up-to-date index. This should only be used if the script `bin/index` cannot be run periodically (using cron for instance). Leave empty to disable.', // @translate
+                'info' => 'The frequency at which the module checks if there are resources to be indexed. A smaller interval means a more up-to-date index. This should only be used if the script `bin/sync` cannot be run periodically (using cron for instance). Leave empty to disable.', // @translate
             ],
             'attributes' => [
                 'id' => 'search-check-interval',
